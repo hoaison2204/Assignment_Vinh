4ef3dd0a00dd992c9e6bd57b6b26f69cc52b5f9d
hoaison2204 <Hoaison2204@gmail.com>
2020-10-20 11:20:40 +0700

    make it better

--- app.js
+++ app.js
@@ -1,62 +1,67 @@
-
-const express = require('express');                 //framework
-const engines = require('consolidate');             //thuc thi cac doan code js der chuyen ve ngon ngu may tinh co the hieu va doc duoc
+const express = require('express');
+const engines = require('consolidate');
 const app = express();
 
+app.engine('hbs',engines.handlebars);
+app.set('views','./views');
+app.set('view engine','hbs');
 
-//npm i handlebars consolidate --save
-app.engine('hbs', engines.handlebars);
-app.set('views', './views');
-app.set('view engine', 'hbs');
-
-var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));            //mot doi tuong body chua du lieu ma da duoc phan tich cu pham se duoc dua vao request. du lieu do la cap key-value, trong do co the la true, array, string neeu extended false, cac loai con lai la true
-
-var publicDir = require('path').join(__dirname, '/public');
+var publicDir = require('path').join(__dirname,'/public');
 app.use(express.static(publicDir));
 
-var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb+srv://hoaison2204:Son22042000@cluster0.cnlb1.mongodb.net/test';
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', async function (req, res) {                        //async khai bao ham bat dong bo - await tam dung ham bat dong bo
-    let client = await MongoClient.connect(url);
-    let dbo = client.db("GCH0719");
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb+srv://truongvinh:vinh1432@cluster0.qdthm.mongodb.net/test';
+
+app.get('/all',async function(req,res){
+    let client= await MongoClient.connect(url);
+    let dbo = client.db("Ass2");
     let results = await dbo.collection("Product").find({}).toArray();
-    res.render('allSanPham', { sanPham: results });
+    res.render('allSanPham',{sanPham:results});
 })
 
-app.get('/delete', async function (req, res) {
+app.get('/add', function(req,res){
+    res.render('addProduct');
+})
+
+app.get('/login',function(req,res){
+    res.render('login');
+})
+
+app.post('/doAddProduct', async function(req,res){
+    let client= await MongoClient.connect(url);
+    let dbo = client.db("Ass2");
+    let Customer = req.body.txtCustomer;
+    let Product = req.body.txtProduct;
+    let Price = req.body.txtPrice;
+    let Quantity = req.body.txtQuantity;
+    let Total = req.body.txtTotal;
+
+    let newProduct = {Customer: Customer,Product:Product,Price:Price,Quantity:Quantity,Total:Total};
+
+    await dbo.collection("Product").insertOne(newProduct);
+    console.log(newProduct);
+
+    res.redirect('/all');
+})
+
+app.get('/delete', async function(req,res){
     let id = req.query.id;
     var ObjectID = require('mongodb').ObjectID;
-    let condition = { "_id": ObjectID(id) };
+    let condition = {"_id":ObjectID(id)};
 
-    let client = await MongoClient.connect(url);
-    let dbo = client.db("GCH0719");
+    let client= await MongoClient.connect(url);
+    let dbo = client.db("Ass2");
     await dbo.collection("Product").deleteOne(condition);
-    res.redirect('/');
+
+    res.redirect('/all');
 })
 
-app.get('/add', function (req, res) {
-    res.render('updateSanPham');
-})
 
-app.post('/doAddproduct', async function (req, res) {
-    let client = await MongoClient.connect(url);
-    var objectID = require('mongodb').objectID;
-
-    let dbo = client.db("GCH0719");
-    let _id = req.body.txt_id;
-    let name = req.body.txtName;
-    let price = req.body.txtPrice;
-
-    let newProduct = { _id : _id, name: name, price: price };
-    await dbo.collection("Product").insertOne(newProduct);
-    // console.log(newProduct);
-
-    res.redirect('/');
-})
 
 const PORT = process.env.PORT || 5000
-var server = app.listen(PORT, function () {
+var server=app.listen(PORT,function() {
     console.log("Server is running at " + PORT);
 });
